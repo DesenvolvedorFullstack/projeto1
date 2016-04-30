816873c750b6e6e78f57e240741d6be9688fe6a0
nataniel <nataniel.paiva@gmail.com>
2016-04-30 02:04:43 -0300

    Subindo o projeto

---
+++ main.js
@@ -0,0 +1,5 @@
+//projeto1/main.js
+var app = angular.module('Projeto1',[]);
+app.controller('PrimeiraController', function($scope) {
+    $scope.mensagem = "Olá, esse é o meu primeiro projeto em AngularJS";
+});
